81b486ebe8bd04761448812d20e4619386f24712
Nikolai Mushegian <hi@nikolai.fyi>
2019-04-19 14:25:14 -0400

    dapp init Names

---
+++ src/Names.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.6;
+
+import "ds-test/test.sol";
+
+import "./Names.sol";
+
+contract NamesTest is DSTest {
+    Names names;
+
+    function setUp() public {
+        names = new Names();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
